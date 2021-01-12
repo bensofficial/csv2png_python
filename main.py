4a3c334656eed66b32d629058345b7724e0a6c85
bensofficial <66966223+bensofficial@users.noreply.github.com>
2021-01-12 14:34:43 +0100

    Updating for own needs

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import os
 
-def main():
-    filelist = os.listdir()
-    for file in filelist:
-        if not os.path.isdir(file) and file[-4:]==".csv":
-            csv2png(file)
-
-
 def csv2png(file):
     df = pd.read_csv(file, index_col=0)
     for i, dat in df.iteritems():
@@ -21,4 +14,4 @@ def csv2png(file):
 
 
 if __name__ == '__main__':
-    main()
+    csv2png('ismaning.csv')
